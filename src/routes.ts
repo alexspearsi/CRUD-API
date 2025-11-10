ce378b7451937b2947c1158d57421983104ed44c
Alex <strelchenko.aleksandr@yandex.ru>
2025-11-10 19:42:20 +0300

    feat: create /api/users with POST method

--- src/routes.ts
+++ src/routes.ts
@@ -1,26 +1,34 @@
 import { IncomingMessage, ServerResponse } from 'http';
+import { v4 as uuidv4, validate as uuidValidate } from 'uuid';
 
-const users = [
+type User = {
+  id?: string,
+  username: string,
+  age: number,
+  hobbies: string[]
+}
+
+const users: User[] = [
   {
-    id: 1,
+    id: '550e8400-e29b-41d4-a716-446655440000',
     username: 'Alex',
     age: 25,
     hobbies: ['drawing', 'basketball']
   },
   {
-    id: 2,
+    id: '550e8400-e29b-41d4-a716-446655440001',
     username: 'Maria',
     age: 30,
     hobbies: ['football']
   },
   {
-    id: 3,
+    id: '550e8400-e29b-41d4-a716-446655440002',
     username: 'John',
     age: 28,
     hobbies: ['swimming']
   },
   {
-    id: 4,
+    id: '550e8400-e29b-41d4-a716-446655440003',
     username: 'Anna',
     age: 22,
     hobbies: ['tennis']
@@ -31,6 +39,7 @@ export function userRouter(req: IncomingMessage, res: ServerResponse) {
   const url = req.url;
   const method = req.method;
 
+  // GET
   if (url === '/api/users' && method === 'GET') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
@@ -38,11 +47,12 @@ export function userRouter(req: IncomingMessage, res: ServerResponse) {
     return;
   }
 
+  // GET USER
   if (url?.startsWith('/api/users/') && method === 'GET') {
     const parts = url.split('/');
-    const userID = Number(parts[3]);
+    const userID = parts[3];
 
-    if (isNaN(userID)) {
+    if (!uuidValidate(userID)) {
       res.statusCode = 400;
       res.setHeader('Content-Type', 'application/json');
       res.end(JSON.stringify({ message: 'Invalid data' }))
@@ -64,6 +74,55 @@ export function userRouter(req: IncomingMessage, res: ServerResponse) {
     res.end(JSON.stringify(user))
     return;
   }
+
+  // POST USER
+  if (url === '/api/users' && method === 'POST') {
+    let body = '';
+
+    req.on('data', chunk => {
+      body += chunk;
+    });
+
+    req.on('end', () => {
+      try {
+        const data = JSON.parse(body);
+
+        const { username, age, hobbies } = data;
+
+        if (
+          typeof username !== 'string' ||
+          typeof age !== 'number' ||
+          !Array.isArray(hobbies)
+        ) {
+          res.statusCode = 400;
+          res.setHeader('Content-Type', 'application/json');
+          res.end(JSON.stringify({ message: 'Missing required fields'}))
+          return;
+        }
+
+        const newUser: User = {
+          username,
+          age,
+          hobbies,
+          id: uuidv4()
+        }
+
+        users.push(newUser);
+
+        res.statusCode = 201;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(newUser));
+      } catch {
+        res.statusCode = 400;
+        res.end(JSON.stringify({ message: 'Invalid JSON'}))
+      }
+ 
+    })
+
+    return;
+  }
+
+
 
   res.statusCode = 404;
   res.setHeader('Content-Type', 'application/json');
