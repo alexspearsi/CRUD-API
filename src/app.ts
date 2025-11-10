284c64dbe247ecd308b4e4fddc0105936eea0f85
Alex <strelchenko.aleksandr@yandex.ru>
2025-11-10 18:50:20 +0300

    feat: create /api/users route

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import { userRouter } from './routes.js';
 
 dotenv.config();
+
 export const PORT = process.env.PORT || 4000;
 
 export const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,49 @@
 import { IncomingMessage, ServerResponse } from 'http';
 
+const users = [
+  {
+    id: 1,
+    name: 'Alex',
+    age: 25,
+    email: 'alex@example.com',
+    isActive: true
+  },
+  {
+    id: 2,
+    name: 'Maria',
+    age: 30,
+    email: 'maria@example.com',
+    isActive: false
+  },
+  {
+    id: 3,
+    name: 'John',
+    age: 28,
+    email: 'john@example.com',
+    isActive: true
+  },
+  {
+    id: 4,
+    name: 'Anna',
+    age: 22,
+    email: 'anna@example.com',
+    isActive: true
+  }
+];
+
 export function userRouter(req: IncomingMessage, res: ServerResponse) {
-  res.write('hello');
-  res.end();
+  const url = req.url;
+  const method = req.method;
+
+  if (url === '/api/users' && method === 'GET') {
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(users));
+    return;
+  }
+
+  res.statusCode = 404;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ message: 'Route not found' }));
 }
